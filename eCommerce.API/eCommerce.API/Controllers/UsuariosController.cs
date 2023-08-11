8f7359e01dfd65c41b126f503cfca93faa573c7c
Cleisson Vieira <cleisson.raimundi@gmail.com>
2023-08-11 17:37:52 -0300

    Finalizada aula 1.5.4 Dapper

---
+++ eCommerce.API/eCommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,63 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_usuarioRepository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _usuarioRepository.Get(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]Usuario usuario)
+        {
+            _usuarioRepository.Insert(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut] 
+        public IActionResult Update([FromBody] Usuario usuario) 
+        {
+            _usuarioRepository.Update(usuario);
+            return Ok(usuario);
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            var usuario = _usuarioRepository.Get(id);
+
+            if (usuario == null)
+                return NotFound();
+            else
+                _usuarioRepository.Delete(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ eCommerce.API/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using eCommerce.API.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+        public UsuarioRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+
+        private static List<Usuario> _db = new()
+        {
+            new Usuario(){ Id = 1, Nome = "Nome usuario 1", Email = "email.usuario1@gmail.com" },
+            new Usuario(){ Id = 2, Nome = "Nome usuario 2", Email = "email.usuario2@gmail.com" },
+            new Usuario(){ Id = 3, Nome = "Nome usuario 3", Email = "email.usuario3@gmail.com" },
+            new Usuario(){ Id = 4, Nome = "Nome usuario 4", Email = "email.usuario4@gmail.com" },
+        };
+
+        public List<Usuario> Get()
+        {
+            return _connection.Query<Usuario>("SELECT * FROM USUARIOS").ToList();
+        }
+
+        public Usuario Get(int id)
+        {
+            return _db.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            var ultimoUsuario = _db.LastOrDefault();
+
+            if (ultimoUsuario == null)
+                usuario.Id = 1;
+            else
+                usuario.Id = ultimoUsuario.Id+1;
+
+            _db.Add(usuario);
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id.Equals(usuario.Id)));
+            _db.Add(usuario);
+        }
+
+        public void Delete(int id)
+        {
+            _db.Remove(_db.FirstOrDefault(x => x.Id.Equals(id)));
+        }
+    }
+}
